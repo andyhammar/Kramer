2362b0f8a0b84d2f2981ece59115b4ad9f523072
Andreas Hammar <andreas.hammar@jayway.com>
2013-03-06 01:03:05 +0100

    moved to src

---
+++ src/Kramer/ViewModels/FeedItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Kramer.ViewModels
+{
+    public class FeedItem
+    {
+        public string Title { get; set; }
+
+        public DateTime PubDate { get; set; }
+
+        public string Author { get; set; }
+
+        public string Content { get; set; }
+    }
+}
