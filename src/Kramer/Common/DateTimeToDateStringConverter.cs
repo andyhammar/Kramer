1cc195509fe2c868f1448aa1f6d3edb0d0433fad
Andreas Hammar <andreas.hammar@jayway.com>
2013-04-08 16:56:27 +0200

    started on win8

---
+++ src/Kramer/Common/DateTimeToDateStringConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using Kramer.Common.Extensions;
+using Windows.UI.Xaml.Data;
+
+namespace Kramer.Common
+{
+    public class DateTimeToDateStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            var date = (DateTime)value;
+            return date.ToSwedishDate();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
