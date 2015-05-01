aed2416d185e9d8ffe4fa4c13bf7a9fa8714a779
Andreas Hammar <andreas.hammar@jayway.com>
2015-05-01 15:17:35 +0200

    started on ads

--- src/Kramer.Phone/App.xaml.cs
+++ src/Kramer.Phone/App.xaml.cs
@@ -1,6 +1,10 @@
-﻿using System.IO.IsolatedStorage;
+﻿using System;
+using System.Diagnostics;
+using System.IO.IsolatedStorage;
 using System.Windows;
+using System.Windows.Markup;
 using System.Windows.Navigation;
+using System.Xml;
 using Kramer.Common.Settings;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
@@ -94,14 +98,53 @@ namespace Kramer.Phone
         // Code to execute on Unhandled Exceptions
         private void Application_UnhandledException(object sender, ApplicationUnhandledExceptionEventArgs e)
         {
+            HandleAdErrors(e);
+            if (e.Handled)
+                return;
+
             LittleWatson.ReportException(e.ExceptionObject, string.Empty);
-            if (System.Diagnostics.Debugger.IsAttached)
+            if (Debugger.IsAttached)
             {
                 // An unhandled exception has occurred; break into the debugger
-                System.Diagnostics.Debugger.Break();
+                Debugger.Break();
             }
         }
 
+        private void HandleAdErrors(ApplicationUnhandledExceptionEventArgs e)
+        {
+            if (e == null) return;
+            Exception exception = e.ExceptionObject;
+            if ((exception is XmlException || exception is NullReferenceException) && exception.ToString().ToUpper().Contains("INNERACTIVE"))
+            {
+                Debug.WriteLine("Handled Inneractive exception {0}", exception);
+                e.Handled = true;
+                return;
+            }
+            else if (exception is NullReferenceException && exception.ToString().ToUpper().Contains("SOMA"))
+            {
+                Debug.WriteLine("Handled Smaato null reference exception {0}", exception);
+                e.Handled = true;
+                return;
+            }
+            else if ((exception is System.IO.IOException || exception is NullReferenceException) && exception.ToString().ToUpper().Contains("GOOGLE"))
+            {
+                Debug.WriteLine("Handled Google exception {0}", exception);
+                e.Handled = true;
+                return;
+            }
+            else if (exception is ObjectDisposedException && exception.ToString().ToUpper().Contains("MOBFOX"))
+            {
+                Debug.WriteLine("Handled Mobfox exception {0}", exception);
+                e.Handled = true;
+                return;
+            }
+            else if ((exception is NullReferenceException || exception is XamlParseException) && exception.ToString().ToUpper().Contains("MICROSOFT.ADVERTISING"))
+            {
+                Debug.WriteLine("Handled Microsoft.Advertising exception {0}", exception);
+                e.Handled = true;
+                return;
+            }
+        }
         #region Phone application initialization
 
         // Avoid double-initialization
@@ -137,7 +180,7 @@ namespace Kramer.Phone
         }
 
         #endregion
-    
+
         System.Windows.Controls.MediaElement _dummyMediaElementForMarketplaceCapabilities = null;
     }
 }
