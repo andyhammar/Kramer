31e559bef1493041bdcafd3bf4a711b36847a5d3
Andreas Hammar <andreas.hammar@jayway.com>
2016-04-18 09:51:06 +0200

    littlewatson works

--- src/_uwp/KramerUwp/KramerUwp.App/LittleWatson.cs
+++ src/_uwp/KramerUwp/KramerUwp.App/LittleWatson.cs
@@ -6,6 +6,7 @@ using System.IO.IsolatedStorage;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.ApplicationModel.Email;
 using Windows.Storage;
 using Windows.UI.Popups;
 
@@ -17,20 +18,20 @@ namespace KramerUwp.App
 
         private const string EMAIL_TO = "ahamapps@live.com";
         private const string EMAIL_SUBJECT = "Echo news (uwp) - error report";
-        const string Filename = "LittleWatson.txt";
-
+        const string FILENAME = "LittleWatson.txt";
+        private static readonly StorageFolder _folder;
 
         static LittleWatson()
         {
-            _filePath = Path.Combine(Path.GetTempPath(), Filename);
+            _folder = ApplicationData.Current.LocalFolder;
         }
 
         internal static async Task ReportException(Exception ex, string extra)
         {
             try
             {
-                var file = await StorageFile.GetFileFromPathAsync(_filePath);
-                
+                var file = await _folder.CreateFileAsync(FILENAME, CreationCollisionOption.OpenIfExists);
+
                 var text = extra + Environment.NewLine + ex.Message + Environment.NewLine + ex.StackTrace;
                 await FileIO.WriteTextAsync(file, text);
             }
@@ -44,29 +45,26 @@ namespace KramerUwp.App
         {
             try
             {
-                var file = await StorageFile.GetFileFromPathAsync(_filePath);
+
+                var file = await _folder.GetFileAsync(FILENAME);
 
                 if (file == null)
                     return;
 
                 var error = await FileIO.ReadTextAsync(file);
 
-                var messageDialog = new MessageDialog("you had error");
+                var messageDialog = new MessageDialog(
+                    "A problem occurred the last time you ran this application. Would you like to send an email to report it?",
+                    "Problem Report");
+                messageDialog.Commands.Add(
+                    new UICommand("Yes", async (x) => await SendEmail(error)));
+                messageDialog.Commands.Add(
+                    new UICommand("No"));
                 await messageDialog.ShowAsync();
 
                 await file.DeleteAsync();
-                //string contents = null;
-                //using (var store = IsolatedStorageFile.GetUserStoreForApplication())
-                //{
-                //    if (store.FileExists(Filename))
-                //    {
-                //        using (TextReader reader = new StreamReader(store.OpenFile(Filename, FileMode.Open, FileAccess.Read, FileShare.None)))
-                //        {
-                //            contents = reader.ReadToEnd();
-                //        }
-                //        //SafeDeleteFile(store);
-                //    }
-                //}
+
+
                 //if (contents != null)
                 //{
                 //    //if (MessageBox.Show(
@@ -97,11 +95,19 @@ namespace KramerUwp.App
             }
         }
 
+        private static async Task SendEmail(string error)
+        {
+            var email = new EmailMessage { Body = error };
+            email.To.Add(new EmailRecipient(EMAIL_TO));
+            email.Subject = EMAIL_SUBJECT;
+            await EmailManager.ShowComposeNewEmailAsync(email);
+        }
+
         //private static void SafeDeleteFile(IsolatedStorageFile store)
         //{
         //    try
         //    {
-        //        store.DeleteFile(Filename);
+        //        store.DeleteFile(FILENAME);
         //    }
         //    catch (Exception)
         //    {
